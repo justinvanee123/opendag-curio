92d5d5a8296144f6ac3b783cc8b8d5b291cecd2c
justinvanee123 <justinvanee123@hotmail.com>
2024-09-04 12:42:41 +0100

    verandering voor nav bar

--- header.php
+++ header.php
@@ -1,9 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responsive Navbar</title>
+    <link rel="stylesheet" href="style.css">
+</head>
 <header>
         <div class="wrapper">
             <h1>curio </h1>
-            <nav>
-                <a href="index.php">home</a>
-                <a href="vakken.php">vakken</a>
-                <a href="docenten.php">docenten</a>
-                <a href="rooster.php">rooster</a>
-            </nav>
+            <div class="navbar">
+        <div class="hamburger" onclick="toggleNavbar()">
+            &#9776; <!-- Hamburger menu icon -->
+        </div>
+        <nav id="myNavbar" class="nav-links">
+            <a href="index.php">home</a>
+            <a href="vakken.php">vakken</a>
+            <a href="docenten.php">docenten</a>
+            <a href="rooster.php">rooster</a>
+        </nav>
+    </div>
+
+    <script src="script.js"></script>
+
